7fdc7f7c7715225fa28255eecfbc501f7f733827
khadijaBahrallil <khadija9932.kb@gmail.com>
2020-10-31 16:51:27 +0100

    Inscription+ Login

---
+++ app/src/main/java/com/ucab/ConducteurMainActivity.java
@@ -0,0 +1,14 @@
+package com.ucab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConducteurMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conducteur_main);
+    }
+}
--- app/src/main/java/com/ucab/MainActivity.java
+++ app/src/main/java/com/ucab/MainActivity.java
@@ -2,13 +2,43 @@ package com.ucab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mConducteur, mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConducteur = (Button) findViewById(R.id.conducteur);
+        mClient = (Button) findViewById(R.id.client);
+
+        mConducteur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginConducteur.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+
+        mClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginClient.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
     }
 }
