63a5b41457ceed3dcb7b1a3d3cee127fd2a77fc5
Cindy <cindyfeliciau@hotmail.fr>
2020-11-24 21:33:31 +0100

    new login

---
+++ app/src/main/java/com/example/ucab_inscription/Register.java
@@ -0,0 +1,127 @@
+package com.example.ucab_inscription;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userId;
+    private String TAG="TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mPhone = findViewById(R.id.phone);
+        mRegisterBtn= findViewById(R.id.registerBtn);
+        mLoginBtn= findViewById(R.id.loginBtn);
+        fAuth=FirebaseAuth.getInstance();
+        progressBar= findViewById(R.id.progressBar);
+        fStore = FirebaseFirestore.getInstance();
+        //Check if the user is already connected or has already an account
+       /* if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }*/
+        //Register the user into the Firebase
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullName=mFullName.getText().toString().trim();
+                final String phone = mPhone.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+
+                if(password.length()<6){
+                    mPassword.setError("Password mist be >= 6 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //If the user is created sucessfully
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User created",  Toast.LENGTH_SHORT).show();
+                            //Store data
+                            userId=fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userId);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fName ",fullName);
+                            user.put("email ",email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d( TAG, "onSuccess: user Profile created for "+ userId);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: "+e.toString());
+                                }
+                            });
+                            //Send the user to the MainActivity
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Error :"+task.getException().getMessage(),  Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+}
