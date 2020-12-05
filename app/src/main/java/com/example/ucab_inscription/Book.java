fa4d1d037b191f493a2ba6d9c4de271f8868a89a
khadijaBahrallil <khadija9932.kb@gmail.com>
2020-12-05 13:39:22 -0500

    paiement fonctionnel + redirection

--- app/src/main/java/com/example/ucab_inscription/Book.java
+++ app/src/main/java/com/example/ucab_inscription/Book.java
@@ -1,43 +1,74 @@
 package com.example.ucab_inscription;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
-import com.checkout.android_sdk.PaymentForm;
-import com.checkout.android_sdk.Response.CardTokenisationFail;
-import com.checkout.android_sdk.Response.CardTokenisationResponse;
+import com.braintreepayments.cardform.view.CardForm;
 
 public class Book extends AppCompatActivity {
-    private PaymentForm mPaymentForm;
+
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
-        PaymentForm.PaymentFormCallback mFormListener = new PaymentForm.PaymentFormCallback() {
+
+        cardForm = findViewById(R.id.card_form);
+        buy = findViewById(R.id.btnBuy);
+
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .setup(Book.this);
+
+        buy.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFormSubmit() {
-                // form submit initiated; you can potentially display a loader
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(Book.this);
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationMonth() + cardForm.getExpirationYear() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(Book.this, "Thank you for purchase", Toast.LENGTH_LONG).show();
+                            Intent in = new Intent(getApplicationContext(),Home.class);
+                            startActivity(in);
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                }else {
+                    Toast.makeText(Book.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
             }
-            @Override
-            public void onTokenGenerated(CardTokenisationResponse response) {
-                // your token is here: response.token
-                mPaymentForm.clearForm(); // this clears the Payment Form
-            }
-            @Override
-            public void onError(CardTokenisationFail response) {
-                // token request error
-            }
-            @Override
-            public void onNetworkError(VolleyError error) {
-                // network error
-            }
-            @Override
-            public void onBackPressed() {
-                // the user decided to leave the payment page
-                mPaymentForm.clearForm(); // this clears the Payment Form
-            }
-        };
+        });
+
     }
 }
